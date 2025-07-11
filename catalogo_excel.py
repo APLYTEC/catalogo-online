c9200036b1fa884df67054b374f8d179ac94ea07
APLYTEC <jguzmanraya@gmail.com>
2025-07-12 00:53:39 +0200

    Add files via upload

--- catalogo_excel.py
+++ catalogo_excel.py
@@ -34,7 +34,8 @@ else:
 # Mostrar los productos
 for _, fila in df_filtrado.iterrows():
     st.markdown(f"### {fila['Nombre']}")
-    st.markdown(f"**Código:** {fila['Código']} &nbsp;&nbsp;&nbsp; **Precio:** {fila['Precio']} €")
+    precio = float(fila['Precio'])
+    st.markdown(f"**Código:** {fila['Código']} &nbsp;&nbsp;&nbsp; **Precio:** {precio:.2f} €")
 
     ruta_imagen = obtener_ruta_imagen(fila["Código"])
     if ruta_imagen:
